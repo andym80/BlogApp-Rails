3863cab8b1eb476680cfe33e6c40c199e5f724ec
andym80 <andymenutti@gmail.com>
2022-12-16 19:07:13 -0300

    connect to db

--- db/migrate/20221216222621_add_reset_password_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddResetPasswordTokenToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :reset_password_token, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_222620) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_222622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_222620) do
     t.string "unconfirmed_email"
     t.datetime "confirmation_sent_at"
     t.string "confirmation_token"
+    t.string "role"
   end
 
   add_foreign_key "comments", "posts", column: "posts_id"
